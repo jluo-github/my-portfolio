db45add44d6c9b2ebb93b5c790fe6fedb53fd17f
jluo <jluo8@wgu.edu>
2024-10-03 19:46:15 -0700

    update data

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         // protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME,
-        // hostname: "*",
+        hostname: "*",
         // port: "",
         // pathname: "",
       },
